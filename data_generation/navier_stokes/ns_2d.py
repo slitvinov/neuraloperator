a54c4dde71314925b5325b60f4bdd519e879a0bb
lisergey <lisergey@penguin>
2023-09-24 20:28:19 -0400

    rm random_fields

--- data_generation/navier_stokes/ns_2d.py
+++ data_generation/navier_stokes/ns_2d.py
@@ -1,8 +1,61 @@
 import torch
 import math
-from random_fields import GaussianRF
-from timeit import default_timer
 import scipy.io
+
+
+class GaussianRF(object):
+
+    def __init__(self,
+                 dim,
+                 size,
+                 alpha=2,
+                 tau=3,
+                 sigma=None,
+                 boundary="periodic",
+                 device=None):
+        self.dim = dim
+        self.device = device
+        if sigma is None:
+            sigma = tau**(0.5 * (2 * alpha - self.dim))
+        k_max = size // 2
+        if dim == 1:
+            k = torch.cat((torch.arange(start=0, end=k_max, step=1, device=device), \
+                           torch.arange(start=-k_max, end=0, step=1, device=device)), 0)
+            self.sqrt_eig = size * math.sqrt(2.0) * sigma * (
+                (4 * (math.pi**2) * (k**2) + tau**2)**(-alpha / 2.0))
+            self.sqrt_eig[0] = 0.0
+        elif dim == 2:
+            wavenumers = torch.cat((torch.arange(start=0, end=k_max, step=1, device=device), \
+                                    torch.arange(start=-k_max, end=0, step=1, device=device)), 0).repeat(size,1)
+            k_x = wavenumers.transpose(0, 1)
+            k_y = wavenumers
+            self.sqrt_eig = (size**2) * math.sqrt(2.0) * sigma * (
+                (4 * (math.pi**2) *
+                 (k_x**2 + k_y**2) + tau**2)**(-alpha / 2.0))
+            self.sqrt_eig[0, 0] = 0.0
+        elif dim == 3:
+            wavenumers = torch.cat((torch.arange(start=0, end=k_max, step=1, device=device), \
+                                    torch.arange(start=-k_max, end=0, step=1, device=device)), 0).repeat(size,size,1)
+            k_x = wavenumers.transpose(1, 2)
+            k_y = wavenumers
+            k_z = wavenumers.transpose(0, 2)
+            self.sqrt_eig = (size**3) * math.sqrt(2.0) * sigma * (
+                (4 * (math.pi**2) *
+                 (k_x**2 + k_y**2 + k_z**2) + tau**2)**(-alpha / 2.0))
+            self.sqrt_eig[0, 0, 0] = 0.0
+        self.size = []
+        for j in range(self.dim):
+            self.size.append(size)
+        self.size = tuple(self.size)
+
+    def sample(self, N):
+        coeff = torch.randn(N,
+                            *self.size,
+                            dtype=torch.cfloat,
+                            device=self.device)
+        coeff = self.sqrt_eig * coeff
+        return torch.fft.ifftn(coeff, dim=list(range(-1, -self.dim - 1,
+                                                     -1))).real
 
 
 def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
@@ -54,6 +107,7 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
             c += 1
     return sol, sol_t
 
+
 device = torch.device('cpu')
 s = 256
 N = 20
@@ -68,7 +122,6 @@ a = torch.zeros(N, s, s)
 u = torch.zeros(N, s, s, record_steps)
 bsize = 20
 c = 0
-t0 = default_timer()
 for j in range(N // bsize):
     print(j)
     w0 = GRF.sample(bsize)
@@ -76,8 +129,7 @@ for j in range(N // bsize):
     a[c:(c + bsize), ...] = w0
     u[c:(c + bsize), ...] = sol
     c += bsize
-    t1 = default_timer()
-    print(j, c, t1 - t0)
+    print(j, c)
 scipy.io.savemat('ns_data.mat',
                  mdict={
                      'a': a.cpu().numpy(),
