9755882c0f839395666087ae084b558287e2b876
Sergey Litvinov <slitvinov@gmail.com>
2023-09-27 19:33:02 -0400

    do not need randn

--- data_generation/darcy/GRF.m
+++ data_generation/darcy/GRF.m
@@ -1,5 +1,5 @@
 function U = GRF(alpha,tau,s)
-  xi = normrnd(0, 1, s);
+  xi = randn(s, s);
   [K1, K2] = meshgrid(0:s-1,0:s-1);
   coef = tau^(alpha-1).*(pi^2*(K1.^2+K2.^2) + tau^2).^(-alpha/2);
   L = s*coef.*xi;
